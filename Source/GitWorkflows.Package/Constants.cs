93c7a8ce1d42e65d846aaa7032ea24b78e839e6d
Bojan Resnik <bojan_resnik@digitalfivesydney.com>
2011-02-01 10:43:32 +1100

    Minor cleanup

--- Source/GitWorkflows.Package/Constants.cs
+++ Source/GitWorkflows.Package/Constants.cs
@@ -18,15 +18,15 @@ namespace GitWorkflows.Package
         public const string guidPackageCmdSetString = "10AD7DBC-5B95-4B38-87F0-4C12590AE6A6";
         public const string guidToolWindowPersistanceString = "459F8AD1-6802-4D74-BD43-86FE92ED898B";
 
-        public static readonly Guid guidPackageCmdSet = new Guid(guidPackageCmdSetString);
-        public static readonly Guid guidPackagePkg = new Guid(guidPackagePkgString);
-        public static readonly Guid guidSccProvider = new Guid(guidSccProviderString);
+        public static readonly Guid guidPackageCmdSet      = new Guid(guidPackageCmdSetString);
+        public static readonly Guid guidPackagePkg         = new Guid(guidPackagePkgString);
+        public static readonly Guid guidSccProvider        = new Guid(guidSccProviderString);
         public static readonly Guid guidSccProviderService = new Guid(guidSccProviderServiceString);
 
-        public const int cmdidCommit =        0x100;
-        public const int cmdidPendingChanges =    0x101;
-        public const int idBranchCombo = 0x401;
-        public const int idBranchComboGetBranches =    0x402;
-        public const int cmdidNewBranch =    0x403;
+        public const int cmdidCommit              = 0x100;
+        public const int cmdidPendingChanges      = 0x101;
+        public const int idBranchCombo            = 0x401;
+        public const int idBranchComboGetBranches = 0x402;
+        public const int cmdidNewBranch           = 0x403;
     };
 }
