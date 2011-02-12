5182bd24ac5b9c5929468655ea6f5acb95f78f0b
Bojan Resnik <bojan_resnik@digitalfivesydney.com>
2011-02-12 16:43:23 +1100

    Removed NotifyPropertyChanged class

--- Source/GitWorkflows.Controls/ViewModels/ViewModel.cs
+++ Source/GitWorkflows.Controls/ViewModels/ViewModel.cs
@@ -1,9 +1,21 @@
-using GitWorkflows.Common;
+using System;
+using System.Linq.Expressions;
+using Microsoft.Practices.Prism.ViewModel;
 
 namespace GitWorkflows.Controls.ViewModels
 {
-    public abstract class ViewModel : NotifyPropertyChanged
+    public abstract class ViewModel : NotificationObject
     {
+        protected bool SetProperty<T>(ref T backingVariable, T newValue, Expression<Func<T>> expression)
+        {
+            if ( Equals(backingVariable, newValue) )
+                return false;
+
+            backingVariable = newValue;
+            RaisePropertyChanged(expression);
+            return true;
+        }
+
 #pragma warning disable 1911
         protected override void RaisePropertyChanged(string propertyName)
         { UIDispatcher.Schedule(() => base.RaisePropertyChanged(propertyName)); }
