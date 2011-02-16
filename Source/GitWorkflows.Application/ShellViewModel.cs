131593c3e82bf97030f8527f4725dadd85017a6d
Bojan Resnik <bojan_resnik@digitalfivesydney.com>
2011-02-17 08:19:01 +1100

    Implemented pending changes view in the standalone application

--- Source/GitWorkflows.Application/ShellViewModel.cs
+++ Source/GitWorkflows.Application/ShellViewModel.cs
@@ -1,10 +1,20 @@
 using System.ComponentModel.Composition;
 using GitWorkflows.Controls.ViewModels;
+using GitWorkflows.Services;
 
 namespace GitWorkflows.Application
 {
     [Export]
     class ShellViewModel : ViewModel
     {
+        [Import]
+        public PendingChangesViewModel PendingChanges
+        { get; private set; }
+
+        [ImportingConstructor]
+        public ShellViewModel(IRepositoryService repositoryService)
+        {
+            repositoryService.OpenRepositoryAt(@"C:\GitProjects\test");
+        }
     }
 }
